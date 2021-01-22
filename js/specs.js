83a6c2de585c133094165fe54ee7ce5f489b8162
Lo-GR <diamondintheroth@gmail.com>
2021-01-21 17:03:02 -0800

    set up start of TDD
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ js/specs.js
@@ -0,0 +1,51 @@
+
+Rules:
+"Take length of message and find square root"
+""
+
+Describe: squareCode(message)
+Test: "Take length of message and find square root"
+Expect(squareCode(25).toEqual(5));
+
+squareCode(message) {
+  const messageLength = Math.sqrt(message.length);
+  if ( 
+}
+
+Test: "Find closest square root of messages without whole number square roots"
+Expect(squareCode(50).toEqual(8,7))
+Test: ""
+Test: ""
+
+
+console.log("This is a test")
+
+
+let stringLength = 37;
+
+let conditional = Math.sqrt(stringLength);
+
+if conditional === whole number
+  conditional is # columns AND # rows
+ else
+  round up to next whole number === # of cols
+  X
+  floor conditional === # rows
+
+  6.xxxxxxxxxxxxx
+7 x 6 = 42 
+
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7
+1 2 3 4 5 6 7
+1 2  
+
+// function isLeapYear(year) {
+  if (year % 4 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
