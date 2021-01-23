c8fd85523b8b60036ddc0f2a423d822f8f7a22de
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-01-23 07:50:01 -0800

    Add business logic to encrypt user's message

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function encrypt(message) {
+  // Make column/row square-code grid
+  const messageStripped = message.toLowerCase().replace(/[\s\W_]/g, "");
+  const columns = Math.ceil(Math.sqrt(messageStripped.length));
+  const rows = Math.ceil(messageStripped.length / columns);
+  let messageArray = messageStripped.split("");
+  let squareCode = [];
+  for (let i = 0; i < rows; i++) {
+    let column = [];
+    squareCode[i] = column;
+    for (let j = 0; j < columns; j++) {
+      column[j] = messageArray.shift();
+    }
+  }
+  // Get cipher characters by going down rows (top to bottom in each column)
+  let cipherArray = [];
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < squareCode.length; j++) {
+      cipherArray.push(squareCode[j][i]);
+    }
+  }
+  // Format cipher to be in blocks of 5 characters separated by a space
+  let cipherClusters = [];
+  while (cipherArray.length > 5) {
+    let cipherSplice = cipherArray.splice(0, 5);
+    cipherClusters = cipherClusters.concat(cipherSplice);
+    cipherClusters.push(" ");
+  }
+  cipher = cipherClusters.concat(cipherArray);
+  
+  return cipher.join("");
+}
